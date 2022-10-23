b920219e9364a69c84d5901dfff4e9399a2dc74d
chud-lori <imlori000@gmail.com>
2022-10-23 21:46:45 +0700

    feat: bubble sort go

---
+++ sorting/bubble.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				// swap
+				arr[j] = arr[j] + arr[j+1]
+				arr[j+1] = arr[j] - arr[j+1]
+				arr[j] = arr[j] - arr[j+1]
+			}
+		}
+	}
+}
+
+func main() {
+	fmt.Println("vim-go")
+	arr := []int{3, 1, 9, 8, 7, 2, 4, 10, 6, 5} // slice
+	bubbleSort(arr)
+	fmt.Println(arr)
+}
