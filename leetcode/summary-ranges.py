1dc2f7ae8bc7722a272bf9824f2df65c1f796de1
chud-lori <imlori000@gmail.com>
2025-05-14 12:40:34 +0700

    feat: summary ranges leetcode

--- leetcode/summary-ranges.py
+++ leetcode/summary-ranges.py
@@ -13,13 +13,36 @@ from typing import List
 #        #print(inter)
 #
 #    return inter
-def summaryRanges(nums: List[int]): -> List[str]:
+def summaryRanges(nums: List[int]) -> List[str]:
+    if not nums:
+        return []
+
+    start = nums[0]
     result = []
+    for i in range(0, len(nums)-1):
+        if(nums[i + 1] != nums[i] + 1):
+            if start == nums[i]:
+                result.append(f"{nums[i]}")
+            else:
+                result.append(f"{start}->{nums[i]}")
+
+            start = nums[i + 1]
+
+    if start == nums[-1]:
+        result.append(str(nums[-1]))
+    else:
+        result.append(f"{start}->{nums[-1]}")
+
+    return result
 
 
 if __name__ == "__main__":
     nums = [0,1,2,4,5,7]
     r = summaryRanges(nums)
     print(r)
-    #assert ["0->2","4->5","7"] == r
+    assert ["0->2","4->5","7"] == r
+    nums = [0,2,3,4,6,8,9]
+    r = summaryRanges(nums)
+    print(r)
+    assert ["0","2->4","6","8->9"] == r
 
